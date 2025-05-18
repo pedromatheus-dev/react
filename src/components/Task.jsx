81126ef2238e485ce486b8f55960da8bab59bc5c
Pedro Matheus <46729924+pedromatheus-dev@users.noreply.github.com>
2025-05-18 16:13:21 -0300

    Adicionando Icone quando Tarefa Concluida

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,4 +1,4 @@
-import {ChevronRightIcon, TrashIcon} from "lucide-react"
+import {CheckIcon, ChevronRightIcon, TrashIcon} from "lucide-react"
 import { useNavigate     } from "react-router-dom"
 import Button from "./Button"
 
@@ -16,7 +16,8 @@ function Tasks(props){
         <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
             {props.tasks.map((task) =>
                 <li key={task.id} className="flex gap-2">
-                    <button onClick={() => props.onTaskClick(task.id)} className={`w-full text-left bg-slate-400 text-white p-2 rounded-md ${task.isCompleted && "line-through"}`}>
+                    <button onClick={() => props.onTaskClick(task.id)} className={`w-full text-left bg-slate-400 text-white p-2 rounded-md flex items-center gap-2 ${task.isCompleted && "line-through"}`}>
+                        {task.isCompleted && <CheckIcon/ >}
                         {task.title}
                     </button>
                     <Button onClick={() => onSeeDetailsClick(task)}>
